4229e1f2a00028fdac1acd04bac7d8cd5faf4e2e
Michael Wu <mikemwu@stanford.edu>
2010-10-15 11:46:51 -0700

    Changed ScalaTest version to 1.2.1-SNAPSHOT
    Updated base project class

--- project/build/DeliteProject.scala
+++ project/build/DeliteProject.scala
@@ -23,16 +23,26 @@ final class DeliteProject(info: ProjectInfo) extends DefaultProject(info) with M
   override def parallelExecution = true
   
   override def testScalaSourcePath = "tests" / "src"
+  override def testResourcesPath = "tests" / "resouces"
+  
+  val virtualization_lms_core = "scala" % "virtualization-lms-core_2.8.x-virtualized-SNAPSHOT" % "0.1"
   
   val scalaToolsSnapshots = ScalaToolsSnapshots
-  val scalatest = "org.scalatest" % "scalatest" %
-    "1.0.1-for-scala-2.8.0.RC1-SNAPSHOT"
+  val scalatest = "org.scalatest" % "scalatest" % "1.2.1-SNAPSHOT"
   
   // Define project class with default source tree layout
   class FlatProject(info: ProjectInfo) extends DefaultProject(info) {
     // Source tree layout
     override def mainScalaSourcePath = "src"
+    
+    override def testScalaSourcePath = "tests" / "src"
+    override def testResourcesPath = "tests" / "resouces"
+    
     val virtualization_lms_core = "scala" % "virtualization-lms-core_2.8.x-virtualized-SNAPSHOT" % "0.1"
+    
+    val scalaToolsSnapshots = ScalaToolsSnapshots
+    val scalatest = "org.scalatest" % "scalatest" % "1.2.1-SNAPSHOT"
+    
     override def localScala =
     defineScala("2.8.x-virtualized-SNAPSHOT", new File(local.scalaVirtualizedHome.get.getOrElse {
       log.error("scala.virtualized.home needs to be defined in local.properties and "+
@@ -49,12 +59,4 @@ final class DeliteProject(info: ProjectInfo) extends DefaultProject(info) with M
   class DSLs(info: ProjectInfo) extends DefaultProject(info) {
     lazy val optiml = project("optiml", "OptiML", new FlatProject(_), framework)
   }
-  
-  lazy val tests = project("tests", "Tests", new Tests(_), framework, runtime, dsls)
-    
-  // Unsure, may have to do sub sub projects
-  class Tests(info: ProjectInfo) extends DefaultProject(info) {
-  }
-  
-  
 }
