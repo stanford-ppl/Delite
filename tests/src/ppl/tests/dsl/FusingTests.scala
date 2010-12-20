6b0b8daced96fe805e89ba9f884fe6066ee3e4b8
asujeeth <asujeeth@stanford.edu>
2010-12-20 13:35:30 -0800

    initial simple test program to drive op fusing experiments

---
+++ tests/src/ppl/tests/dsl/FusingTests.scala
@@ -0,0 +1,28 @@
+package ppl.tests.dsl
+
+import ppl.delite.framework.DeliteApplication
+import ppl.dsl.optiml.{OptiMLExp}
+
+object FusingTests extends DeliteApplication with OptiMLExp {
+
+  def fuseSimple2() {
+    val a = Vector.range(1,10)
+    val b = Vector.range(10,20)
+    val c = Vector.range(20,30)
+    val res = a+b+c
+    res.pprint
+  }
+
+  def fuseMix2() {
+    val a = Vector.range(1,10)
+    val b = Vector.range(10,20)
+    val c = Vector.range(20,30)
+    val res = (a+b).t-c.t
+    res.pprint
+  }
+
+  def main() = {
+    fuseSimple2()
+    fuseMix2()
+  }
+}
