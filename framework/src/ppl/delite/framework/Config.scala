77e6893f322f67eb74a97278cdb702c77e9cdf51
hchafi <hchafi@stanford.edu>
2011-01-08 01:39:02 -0800

    fixup data structure emission

--- framework/src/ppl/delite/framework/Config.scala
+++ framework/src/ppl/delite/framework/Config.scala
@@ -3,4 +3,5 @@ package ppl.delite.framework
 object Config {
   val build_dir = System.getProperty("delite-build-dir", "generated/")
   val deg_filename = System.getProperty("delite-deg-filename", "")
+  val deliteHome: String = System.getProperty("delite-home", System.getProperty("user.dir"))
 }
