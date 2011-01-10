aed8aab90ec9b7ff5692c70477abe171fef53027
hchafi <hchafi@stanford.edu>
2011-01-09 17:00:31 -0800

    number collection and gathering in good enough condition

--- benchmark/generate-dashboard.py
+++ benchmark/generate-dashboard.py
@@ -19,12 +19,48 @@ labelStyle = easyxf(
     'alignment: horizontal center;'
 )
 
+headerAppNameStyle = easyxf(
+    'font: name Arial, colour dark_blue, bold true;'
+    #'borders: left thick, right thick, top thick, bottom thick;'
+    #'pattern: pattern solid, fore_colour red;'
+    'alignment: horizontal center;'
+)
+
+headerRuntimeOddStyle = easyxf(
+    'font: name Arial, colour black, bold true;'
+    #'borders: left thick, right thick, top thick, bottom thick;'
+    'pattern: pattern solid, fore_colour sky_blue;'
+    'alignment: vertical center, horizontal center, rotation 90;'
+)
+
+headerRuntimeEvenStyle = easyxf(
+    'font: name Arial, colour black, bold true;'
+    #'borders: left thick, right thick, top thick, bottom thick;'
+    'pattern: pattern solid, fore_colour light_blue;'
+    'alignment: vertical center, horizontal center, rotation 90;'
+)
+
+headerSystemOddStyle = easyxf(
+    'font: name Arial, colour black, bold true;'
+    #'borders: left thick, right thick, top thick, bottom thick;'
+    'pattern: pattern solid, fore_colour ice_blue;'
+    'alignment: vertical center, horizontal center, rotation 90;'
+)
+
+separatorStyle = easyxf(
+    #'borders: left thick, right thick, top thick, bottom thick;'
+    'pattern: pattern fine_dots, fore_colour light_orange;'
+)
+
+
 # this is some configuration for where to 
 # start printing headers and so forth
 hdr_r_start = 0
 hdr_y_width = 3
 app_data_pts = 2
 
+sequentialTimes = {}
+delite1Times = {}
 
 ##
 # prints the Dashboard Excel Header
@@ -46,14 +82,9 @@ def printExcelHeader(sheet, apps):
         r2 = r1
         c1 = hdr_y_width+appIdx*app_data_pts
         c2 = c1 + app_data_pts - 1
-        sheet.write_merge(r1, r2, c1, c2, app, easyxf(
-            'font: name Arial, colour dark_blue, bold true;'
-            #'borders: left thick, right thick, top thick, bottom thick;'
-            #'pattern: pattern solid, fore_colour red;'
-            'alignment: horizontal center;'
-        ))          
-        sheet.row(r1+1).set_cell_text(c1, 'del. 2.0', labelStyle)
-        sheet.row(r1+1).set_cell_text(c1+1, 'del. 1.0', labelStyle)        
+        sheet.write_merge(r1, r2, c1, c2, app, headerAppNameStyle)          
+        sheet.row(r1+1).set_cell_text(c1, 'time', labelStyle)
+        sheet.row(r1+1).set_cell_text(c1+1, 'speedup', labelStyle)        
         appIdx +=1
     #print table header
     row = sheet.row(hdr_r_start+2)
@@ -105,8 +136,11 @@ def main():
         print "Processing Times Directory: " + timesFolderDate + "\n======================================="
     # process times for each apps in this folder
     
+    #TFLOP section
+    dataSheet.write_merge(rowIdx, rowIdx + len(procs)*2, hdr_y_width - 3, hdr_y_width - 3, "TFLOP", headerSystemOddStyle)
     
-    #Delite section
+    #Delite 2.0 section
+    dataSheet.write_merge(rowIdx, rowIdx + len(procs) - 1, hdr_y_width - 2, hdr_y_width - 2, "Delite 2.0", headerRuntimeOddStyle)
     appIdx = 0
     for app in apps:
         # load application 
@@ -120,18 +154,59 @@ def main():
                 dataSheet.row(app_row).write(hdr_y_width - 1, str(proc) + " CPU")
             appFile = open(dataPath + timesFolderDate + "/" + app + "-smp-" + str(proc) + ".times", 'r')
             numbers = appFile.readlines()
+            appFile.close()
             # convert to times
             times = [float(t) for t in numbers]
             times = times[5:len(numbers)]
             mean = numpy.mean(times)
             min = numpy.amin(times)
             max = numpy.amax(times)
+            if proc == 1:
+                sequentialTimes[app] = mean
             app_col = hdr_y_width + appIdx * app_data_pts
             
             dataSheet.row(app_row).write(app_col, mean)
-            dataSheet.row(app_row).write(app_col + 1, 'n/a')
+            dataSheet.row(app_row).write(app_col + 1, sequentialTimes[app]/mean)
             procIdx += 1
         appIdx += 1
+    rowIdx += len(procs)
+    
+    #Separator
+    dataSheet.write_merge(rowIdx, rowIdx, hdr_y_width - 2, hdr_y_width + len(apps)*2 - 1, "", separatorStyle)
+    rowIdx += 1
+    
+    #Delite 1.0 section
+    dataSheet.write_merge(rowIdx, rowIdx + len(procs) - 1, hdr_y_width - 2, hdr_y_width - 2, "Delite 1.0", headerRuntimeEvenStyle)
+    
+    if options.verbose:
+        print "**************************************\nGenerating Delite 1.0 Numbers\n******************************"
+    #load the times
+    f = open(dataPath +  "delite1.times", 'r')
+    for line in f:
+        tokens = line.split(' ')
+        app = tokens.pop(0)
+        delite1Times[app] = {}  
+        for p in procs:
+            time = tokens.pop(0)
+            delite1Times[app][p] = float(time)            
+    f.close()
+    appIdx = 0
+    for app in apps:
+        # load application 
+        if options.verbose:
+            print "Loading times for application: " + app
+        procIdx = 0
+        for proc in procs:
+            app_row = rowIdx + procIdx
+            # put num threads in column
+            if appIdx == 0:
+                dataSheet.row(app_row).write(hdr_y_width - 1, str(proc) + " CPU")        
+            app_col = hdr_y_width + appIdx * app_data_pts            
+            dataSheet.row(app_row).write(app_col, delite1Times[app][proc])
+            dataSheet.row(app_row).write(app_col + 1, delite1Times[app][1]/delite1Times[app][proc])
+            procIdx += 1
+        appIdx += 1
+    rowIdx += len(procs)
         
     if options.verbose:
         print "**************************************\nGenerating dashboard excel spreadsheet to %s..." % xlFile    
