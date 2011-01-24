7ec83bd5f07f129c2aeb9c6f9844924b86ec073c
hchafi <hchafi@stanford.edu>
2011-01-24 01:25:58 -0800

    improvements to gather script

--- benchmark/gather-numbers.py
+++ benchmark/gather-numbers.py
@@ -35,6 +35,7 @@ def main():
     parser.add_option("-r", "--runs", dest="runs", default="10", type="string", help="the number of times the runtime will execute the applications")
     parser.add_option("-t", "--threads", dest="threads", default="_all", help="a list of comma separated thread counts (e.g. -t 1,2,4)")
     parser.add_option("-s", "--skip", dest="skip", default="_none", help="skips smp and/or gpu portion of gathering numbers (e.g. -s gpu)")
+    parser.add_option("--nv", dest="no_variants", action="store_true" , help="disables variant support in the framework")
 
     (opts, args) = parser.parse_args()
     if len(args) != 0:
@@ -47,32 +48,6 @@ def main():
     launchApps(options)
     
 
-#        if app.find("gpu") < 0:
-#            for procnum in procnumbers:
-#                launchApp(app, int(procnum), options)
-#        else:
-#            print launchApp(app, 1, options)
-
-#def setApplicationVariables(procnum, nativeOps):
-    #if procnum == -1:
-     #   optsString = "-DenableDebugOptions=true -DbypassExecutor=true"
-        #os.putenv("DELITE_NUM_THREADS", `1`)
-    #else:
-     #   optsString = ""
-        #os.putenv("DELITE_NUM_THREADS", `procnum`)
-
-    #if nativeOps:
-    #    print "Enabling native ops."
-    #    optsString = optsString + " -DuseNativeLibs=true"
-    #else:
-    #    print "Disabling native ops."
-    #    optsString = optsString + " -DuseNativeLibs=false"
-    #os.putenv("JAVA_OPTS", os.getenv("JAVA_OPTS", "") + " " + optsString)
-
-#def launchApp(app, procnum, options):
-    #setApplicationVariables(procnum, options.native)
-    #os.putenv("DELITE_BASE", CONFIG_DIR + "/../../")
-    #os.system(CONFIG_DIR + "/" + app + params[app])
 
 def loadOptions(opts):
     options['verbose'] = opts.verbose
@@ -95,6 +70,7 @@ def loadOptions(opts):
     if(opts.skip != "_none"):
         for s in opts.skip.split(','):
             options['run'][s] = False
+    options['variants'] = not opts.no_variants
 
 def loadProps(options):
     #load and check all the required environment variables
@@ -114,8 +90,13 @@ def loadProps(options):
 def launchApps(options):
     # run the delite applications
     for app in options['apps']:
-        print "Running app: " + app + "\n===========================\nGenerating DEG file" 
-        os.putenv("GEN_OPTS", "-Dblas.home=" + props['intel.mkl'] + " -Ddelite.home.dir=" + DELITE_HOME + " -Ddelite.build.dir=" + DELITE_HOME +  "/generated/ -Ddelite.deg.filename=" + app + ".deg")
+        print "==================================================="
+        print "==         " + app 
+        print "===================================================" 
+        opts = "-Dblas.home=" + props['intel.mkl'] + " -Ddelite.home.dir=" + DELITE_HOME + " -Ddelite.build.dir=" + DELITE_HOME +  "/generated/ -Ddelite.deg.filename=" + app + ".deg"
+        if options['variants'] == False:
+            opts = opts + " -Dnested.variants.level=0"
+        os.putenv("GEN_OPTS", opts)
         #MKL ENV
         os.putenv("LD_PRELOAD", props['java.home'] + "/jre/lib/amd64/libjsig.so")
         #emit other envs
@@ -123,7 +104,11 @@ def launchApps(options):
         os.putenv('LMS_HOME', props['libs.lms.home'])
         os.putenv('SCALA_VIRT_HOME', props['scala.virtualized.home'])
         os.putenv('PATH', props['intel.icc'] + ":" + os.getenv('PATH'))
-        os.system(DELITE_HOME + "/bin/gen " + classes[app])
+        print "==  Generating DEG file with options: " + opts
+        ecode = os.system(DELITE_HOME + "/bin/gen " + classes[app])
+        if ecode != 0:
+            print "Detected abnormal exit code, exiting"
+            exit(-1)
         #do it for each config of delite
         #do it for each thread configuration
         if options['run']['smp']: 
@@ -134,18 +119,25 @@ def launchApps(options):
                 print "running: " + app + " " + params[app],
                 print "with config: " + opts + "\n"
                 os.putenv("SCALA_HOME", props['scala.vanilla.home'])
-                os.system(DELITE_HOME + "/bin/exec " + app + ".deg " + params[app])
+                ecode = os.system(DELITE_HOME + "/bin/exec " + app + ".deg " + params[app])
+                if ecode != 0:
+                    print "Detected abnormal exit code, exiting"
+                    exit(-1)
+
         #check if gpu option is enabled
         if options['run']['gpu']:
-            opts = "-Ddelite.home=" + DELITE_HOME +" -Ddelite.threads=1 -Ddelite.gpus=1 -Ddelite.scheduler=GPUOnlyStaticScheduler -Ddelite.executor=SMP+GPUExecutor" + " -Ddelite.runs=" + options['runs'] + " -Dstats.dump -Dstats.dump.component=app -Dstats.dump.overwrite -Dstats.output.dir=" + DELITE_HOME  + "/benchmark/times -Dstats.output.filename=" + app + "-gpu.times"         
+            opts = "-Ddelite.home=" + DELITE_HOME +" -Ddelite.threads=1 -Ddelite.gpus=1 -Ddelite.runs=" + options['runs'] + " -Dstats.dump -Dstats.dump.component=app -Dstats.dump.overwrite -Dstats.output.dir=" + DELITE_HOME  + "/benchmark/times -Dstats.output.filename=" + app + "-gpu.times"         
             os.putenv("JAVA_OPTS", os.getenv("JAVA_OPTS", "") + " " + opts)
             os.putenv("MKL_NUM_THREADS", "1")
             #need nvcc in your path
             os.putenv('PATH', props['nvidia.cuda'] + "/bin:" + os.getenv('PATH'))
-            print "running: " + app + " " + params[app],
-            print "with config: " + opts + "\n"
+            print "== executing application: " + app + " " + params[app],
+            print "== with options: " + opts + "\n"
             os.putenv("SCALA_HOME", props['scala.vanilla.home'])
-            os.system(DELITE_HOME + "/bin/exec " + app + ".deg " + params[app])
+            ecode = os.system(DELITE_HOME + "/bin/exec " + app + ".deg " + params[app])
+            if ecode != 0:
+                print "Detected abnormal exit code, exiting"
+                exit(-1)
  		
 
 
