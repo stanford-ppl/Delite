a84d104b62a5de9ca1b7afe6d13be97523df83c7
Michael Wu <mikemwu@stanford.edu>
2011-01-28 11:51:55 -0500

    application data setting

--- dsls/optiml/src/ppl/dsl/optiml/datastruct/scala/ApplicationImpls.scala
+++ dsls/optiml/src/ppl/dsl/optiml/datastruct/scala/ApplicationImpls.scala
@@ -13,7 +13,7 @@ class DenoiseVertexDataImpl(val _id : Int, var _belief : Vector[Double], var _po
   def id = _id
 
   def belief = _belief
-  def setBelief(b: Vector[Double]) = {
+  def belief_=(b: Vector[Double]) = {
     _belief = b
   }
 
@@ -22,12 +22,12 @@ class DenoiseVertexDataImpl(val _id : Int, var _belief : Vector[Double], var _po
 
 class DenoiseEdgeDataImpl(var _msg : Vector[Double], var _oldMsg : Vector[Double]) extends DenoiseEdgeData {
   def message = _msg
-  def setMessage(msg: Vector[Double]) = {
+  def message_=(msg: Vector[Double]) = {
     _msg = msg
   }
 
   def oldMessage = _oldMsg
-  def setOldMessage(msg: Vector[Double]) = {
+  def oldMessage_=(msg: Vector[Double]) = {
     _oldMsg = msg
   }
 
