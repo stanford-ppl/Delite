ef54d2f374821f6671995cecc5fb624b3c4d6697
Tiark Rompf <tiark.rompf@epfl.ch>
2011-01-30 01:41:44 +0100

    number of fat loops in svm down to 16

--- dsls/optiml/src/ppl/dsl/optiml/VectorOps.scala
+++ dsls/optiml/src/ppl/dsl/optiml/VectorOps.scala
@@ -827,6 +827,7 @@ trait VectorOpsExpOpt extends VectorOpsExp {
     case Def(VectorObjectZeros(l)) => unit(0).asInstanceOf[Exp[A]]
     case Def(VectorObjectOnes(l)) => unit(1).asInstanceOf[Exp[A]]
     case Def(VectorObjectRange(s,e,d,r)) => (s + n*d).asInstanceOf[Exp[A]]
+    case Def(VectorTrans(x)) => vector_apply(x,n)
     case _ => super.vector_apply(x,n)
   }
   
