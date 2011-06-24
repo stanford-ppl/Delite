a77e542a9dfdd62032ae88302e62e9c8e7504cf3
Michael Wu <mikemwu@stanford.edu>
2011-06-23 20:58:27 -0700

    fixed tests, made sbt use delite.properties since we have to define it anyways, removed local.properties.sample

--- project/build/DeliteProject.scala
+++ project/build/DeliteProject.scala
@@ -2,19 +2,19 @@ import sbt._
 import java.io.File
 
 final class DeliteProject(info: ProjectInfo) extends DefaultProject(info) with MavenStyleScalaPaths {
-  // define a root-level environment file local.properties
+  // define a root-level environment file delite.properties
   // from which scala.virtualized.home and virtualization_lms_core.home will be read
   // to get the directory of scala virtualized and the virtualization-lms-core
   lazy val local = new BasicEnvironment {
       def log = DeliteProject.this.log
-      def envBackingPath = info.projectPath / "local.properties"
+      def envBackingPath = info.projectPath / "delite.properties"
       lazy val scalaVirtualizedHome = property[String]
   }
   
   // use the local scala-virtualized compiler and library
   override def localScala =
     defineScala("2.9.x-virtualized-SNAPSHOT", new File(local.scalaVirtualizedHome.get.getOrElse {
-      log.error("scala.virtualized.home needs to be defined in local.properties and "+
+      log.error("scala.virtualized.home needs to be defined in delite.properties and "+
       "must point to a valid scala-virtualized home directory"); "<undefined>"
     }))::Nil
 
@@ -43,7 +43,7 @@ final class DeliteProject(info: ProjectInfo) extends DefaultProject(info) with M
     
     override def localScala =
     defineScala("2.9.x-virtualized-SNAPSHOT", new File(local.scalaVirtualizedHome.get.getOrElse {
-      log.error("scala.virtualized.home needs to be defined in local.properties and "+
+      log.error("scala.virtualized.home needs to be defined in delite.properties and "+
       "must point to a valid scala-virtualized home directory"); "<undefined>"
     }))::Nil 
   }
@@ -71,5 +71,5 @@ final class DeliteProject(info: ProjectInfo) extends DefaultProject(info) with M
   //TR is anybody using this? conflict with defining 'tests' as test source path above...
   //aks: i am, but i'm in the process of trying to convert all the remaining tests to be scalatests.
   //in the meantime, i tried to set this up so it wouldn't conflict with the existing scalatests
-  lazy val tests = project("tests", "Delite Tests", new FlatProject(_), framework, dsls, apps)
+  //lazy val tests = project("tests", "Delite Tests", new FlatProject(_), framework, dsls, apps)
 }
