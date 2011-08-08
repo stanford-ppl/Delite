abdd967d1dacbbd8b6441d369ead54b85d70abf0
Michael Wu <mikemwu@stanford.edu>
2011-08-07 21:06:18 -0700

    hello world

---
+++ apps/scala/src/ppl/apps/liszt/hello_world/Main.scala
@@ -0,0 +1,25 @@
+package ppl.apps.ml.liszt.hello_world
+
+import ppl.dsl.deliszt.datastruct.scala._
+import ppl.dsl.deliszt.{DeLisztApplicationRunner, DeLisztApplication}
+
+object HelloWorldRunner extends DeLisztApplicationRunner with HelloWorld
+
+trait HelloWorld extends DeLisztApplication {
+    val field = FieldWithConst[Cell,Int](0)
+    val ffield = FieldWithConst[Face,Int](0)
+    def main() {
+		for(c <- cells(mesh)) {
+      field(c) += 1
+		}
+		for(f <- faces(mesh)) {
+			val c = if(ID(f) == 4) inside(f) else outside(f)
+			// val v = if(ID(c) == 3) vertex(c,0) else vertex(c,1)
+			// Print(v)
+			for (ce <- cells(f)) {
+				field(ce) += 10
+				ffield(f) += 10
+			}
+		}
+    }
+}
