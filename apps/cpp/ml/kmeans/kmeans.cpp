af55e2c01e85a1712793c748222de32dc33ddd23
Lin Huang <linhuang@tflop1.Stanford.EDU>
2011-10-25 20:20:14 -0700

    implement kmean without armadillo

--- apps/cpp/ml/kmeans/kmeans.cpp
+++ apps/cpp/ml/kmeans/kmeans.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
+#include <fstream>
 #include <stdlib.h>
+#include <string>
 #include <math.h>
 #include <limits>
 #include <ml/OptiML.hpp>
-#include "armadillo"
 
-using std::cout;
-using namespace arma;
+using namespace std;
 
 double tol = 0.001; // tolerance
 
@@ -15,117 +15,138 @@ void print_usage() {
   exit(-1);
 }
 
-int findNearestCluster(const rowvec& x_i, const mat& mu ) {
-  double min_d = std::numeric_limits<double>::max();
-  int min_j = -1;
+void constructNearestClusterVector(double *v, double **x, int x_row, int x_col, double **mu, int mu_row, int mu_col) {
+  int i, j, k;
+  double dist, min_d;
+  int min_j;
 
-  for(uint j = 0; j < mu.n_rows; j++) {
-    uint l = x_i.n_cols;
-    vec  v(l);
-    for(uint i = 0; i < l; i++) {
-      rowvec r = mu.row(j);
-      v.at(i) = pow(x_i.at(i) - r.at(i), 2);
+  for(i = 0; i < x_row; i++) {
+    min_d = -1, min_j = -1;
+    for(j = 0; j < mu_row; j++) {
+      dist = 0;
+      for(k = 0; k < x_col; k++) {
+        dist += (x[i][k] - mu[j][k]) * (x[i][k] - mu[j][k]);
+      }
+      if(min_d < -0.5 || dist < min_d) min_d = dist, min_j = j;
     }
-    
-    int dist = sum(v);
-    if(dist < min_d) {
-      min_d = dist;
-      min_j = j;
-    }
-    
+    v[i] = min_j;
   }
-
-  return min_j;
-
 }
 
-void constructNearestClusterVector(ivec &v, const mat& x, const mat& mu) {
-  for(uint i = 0; i < v.n_rows; i++) {
-    v.at(i) = findNearestCluster(x.row(i), mu);
+double ** ReadInputFile(char * filename, int & p_row, int & p_col) {
+  int i, j;
+  double **p;
+  int row = 0, col = 0;
+  char chr, pre_chr = 'a';
+  bool start = false;
+
+  ifstream infile(filename);
+  infile>>noskipws;
+
+  while(infile>>chr) {
+    if(!start && chr != ' ') start = true;
+    if(chr == '\n') ++row, start = false;
+    if(start && chr == ' ' && pre_chr != ' ' && !row) ++col;
+    pre_chr = chr;
   }
+  infile.close();
+  col++;
+  p_row = row, p_col = col;
+
+  if((p = (double**)malloc(row * sizeof(double*))) == NULL) cout<<"Cannot allocate memory";
+  for(i = 0; i < row; i++) {
+    if((p[i] = (double*)malloc(col * sizeof(double))) == NULL) cout<<"Cannot allocate memory";
+  }
+
+  ifstream datafile(filename);
+  i = 0, j = 0;
+  while(i < row && j < col) {
+    datafile >> p[i][j];
+    j++;
+    if(j == col) i++, j = 0;
+  }
+  datafile.close();
+
+  return p;
 }
 
 int main(int argc,char *argv[]) {
+  double **x, **mu;
+  int x_row, x_col, mu_row, mu_col;
+  int i, j, k;
+  int iter = 0;
 
   if(argc != 3) {
     print_usage();
   }
 
-  mat x = OptiML::armad::MLInputReader::readDoubleMatrix(argv[1]);
-  mat mu =  OptiML::armad::MLInputReader::readDoubleMatrix(argv[2]);
-  int m = x.n_rows;
-  int k = mu.n_rows;
-  int iter = 0;
+  x = ReadInputFile(argv[1],x_row, x_col);
+  mu = ReadInputFile(argv[2],mu_row, mu_col);
 
-  mat oldmu(mu.n_rows, mu.n_cols);
+  double **oldmu;
+  if((oldmu = (double**)malloc(mu_row * sizeof(double*))) == NULL) cout<<"Cannot allocate memory for oldmu";
+  for(i = 0; i < mu_row; i++) {
+    if((oldmu[i] = (double*)malloc(mu_col * sizeof(double))) == NULL) cout<<"Cannot allocate memory for oldmu";
+  }
   
   cout << "kmeans starting computation.\n";
   
   OptiML::tic();
   
   double diff;
+  double *c;
+  if((c = (double*)malloc(x_row * sizeof(double))) == NULL) cout<<"Cannot allocate memory for c";
+  double *weightedPoints;
+  if((weightedPoints = (double*)malloc(x_col * sizeof(double))) == NULL) cout<<"Cannot allocate memory for weightedPoints";
+
   do {
     iter++;
-    oldmu = mu;
     
-    ivec c(m);
-    constructNearestClusterVector(c, x, mu);
-
-    //    irowvec c_t = trans(c);
-    //    c_t.print("c = ");
+    for(i = 0; i < mu_row; i++) 
+      for(j = 0; j < mu_col; j++)
+        oldmu[i][j] = mu[i][j];
     
-    rowvec weightedPoints(x.n_cols);
-    for(int j = 0; j < k; j++) {
-      weightedPoints.zeros();
+    constructNearestClusterVector(c, x, x_row, x_col, mu, mu_row, mu_col);
+    
+    for(i = 0; i < mu_row; i++) {
+      for(j = 0; j < x_col; j++) weightedPoints[j] = 0;
       int points = 0;
-      for(int i = 0; i < m; i++) {
-	if(c(i) == j) {
-	  weightedPoints = weightedPoints +  x.row(i);
-	  points++;
-	}
+      for(j = 0; j < x_row; j++) {
+        if(fabs(c[j] - i) < 1e-8) {
+          for(k = 0; k < x_col; k++) weightedPoints[k] += x[j][k];
+          points++;
+        }
       }
-      if(points == 0) {
-	points++;
-      }
-      //weightedPoints.print("wp = ");
-      //cout << "points: " << points << endl;
-      OptiML::armad::Matrix::setMatrixRowToVector(mu,j, weightedPoints/points);
-
+      if(!points) points++;
+      for(j = 0; j < mu_col; j++) mu[i][j] = weightedPoints[j] / points;
     }
-    mat mdiff = OptiML::armad::Matrix::abs(mu-oldmu);
-    diff = OptiML::armad::Matrix::sum(mdiff);
+
+    diff = 0;  
+    for(i = 0; i < mu_row; i++)
+      for(j = 0; j < mu_col; j++)
+        diff += fabs(mu[i][j] - oldmu[i][j]);
+
   } while (diff > tol);
+
+  for(i = 0; i < x_row; i++) free(x[i]);
+  free(x);
+  for(i = 0; i < mu_row; i++) free(oldmu[i]);
+  free(oldmu);
+  free(c);
+  free(weightedPoints);
 
   OptiML::toc();
   cout << "finished in " << iter << " iterations.\n";
-  mu.print("mu = ");
+  for(i = 0; i < mu_row; i++) {
+    for(j = 0; j < mu_col; j++)
+      cout<<mu[i][j]<<"  ";
+    cout<<endl;
+  }
+
+  for(i = 0; i < mu_row; i++) free(mu[i]);
+  free(mu);
+
   return 0;
 
 }
 
-
-// //NOTE: this function returns an Int: need to be careful
-// def findNearestCluster( x_i: DoubleVector, mu: DoubleMatrix ) : Int = {
-//      var min_d = Double.PositiveInfinity
-//      var min_j = -1
-
-//      for (j <- 0 until mu.height) {
-
-//        val l = x_i.length
-//        val v =  DoubleVector(l)
-//        var idx = 0
-//        while (idx != l) {
-//          v(idx) = Math.pow(x_i(idx)- mu(j)(idx),2)
-//          idx += 1
-//        }
-
-//        val dist = v.sum
-
-//        if (dist < min_d){
-//          min_d = dist
-//          min_j = j
-//        }
-//      }
-
-//      return min_j
-// }
