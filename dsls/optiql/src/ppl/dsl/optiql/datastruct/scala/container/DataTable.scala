96874329a0b8081b9a841df4d79660afae9e0a41
Kevin Brown <kjbrown@tflop1.Stanford.EDU>
2011-11-01 23:37:44 -0700

    added printAsTable for SoA form

--- dsls/optiql/src/ppl/dsl/optiql/datastruct/scala/container/DataTable.scala
+++ dsls/optiql/src/ppl/dsl/optiql/datastruct/scala/container/DataTable.scala
@@ -7,10 +7,128 @@ import ppl.dsl.optiql.datastruct.scala.util.{ReflectionHelper, Date}
 object DataTable {
 
   def printAsTable(m: Map[String,Any]) {
-    println("TODO: output formatting")
-    println("data table: " + m)
+    // Check if Table is empty
+    if(m.size == 0) {
+      println("=====================================================")
+      println("|                  EMPTY TABLE                      |")
+      println("=====================================================")
+      return
+    }
+
+    implicit val tableStr = new StringBuilder
+    val numRows = m("size").asInstanceOf[Int]
+    val fields = m("data").asInstanceOf[Map[String,Any]]
+
+    val columnSizes = getTableColSizes(fields)
+
+    def repeat(s: String, n:Int) {
+      //assert(n < 0 || n > 300, "Incorrect value supplied for n in repeat")
+      //todo for now, just ignore bad value of n
+      if(n < 0 || n > 300)
+        return
+      var idx = 0
+      while(idx != n) {
+        tableStr.append(s);
+        idx += 1
+      }
+    }
+
+    def horizontalRule = {
+      for(i <- 0 until columnSizes.size )
+        repeat("=",columnSizes(i)+1)
+      tableStr append("=\n")
+    }
+
+    def newLine = {
+      tableStr append("\n")
+    }
+
+    horizontalRule
+    tableStr append("|")
+    val fieldStrs = fields.keys.toArray
+    for(i <- 0 until columnSizes.size) {
+      tableStr append( " " + fieldStrs(i))
+      repeat(" " , columnSizes(i) - fieldStrs(i).size - 1  )
+      tableStr append("|")
+    }
+    newLine
+    horizontalRule
+    print(tableStr.toString)
+    tableStr.clear
+
+    for(r <- 0 until numRows) {
+      emitRecordAsRow(r, columnSizes)
+    }
+
+    print(tableStr.toString)
+    tableStr.clear
+
+    horizontalRule
+    println(tableStr.toString)
+
+    def emitRecordAsRow(r: Int, columnSizes: Array[Int]) {
+      tableStr append("| ")
+      var str = ""
+
+      var idx = 0
+      for(f <- fields.keys) {
+        str = readArray(fields(f), r)
+        tableStr append(str); repeat(" ", columnSizes(idx) - str.size - 1); tableStr append("| ")
+        idx += 1
+      }
+      tableStr append("\n")
+    }
   }
-  
+
+  private def max(a: Int, b: Int) = if(a > b) a else b
+
+  private def readArray(a: Any, idx: Int) = a match {
+    case i: Array[Int] => i(idx).toString
+    case l: Array[Long] => l(idx).toString
+    case d: Array[Double] => d(idx).toString
+    case f: Array[Float] => f(idx).toString
+    case c: Array[Char] => c(idx).toString
+    case s: Array[Short] => s(idx).toString
+    case b: Array[Byte] => b(idx).toString
+    case z: Array[Boolean] => z(idx).toString
+    case r: Array[AnyRef] => r(idx).toString
+    case ar: AnyRef => throw new IllegalArgumentException(ar.getClass.getSimpleName + " cannot be printed as a table")
+  }
+
+  private def arrayToString(a: Any) = a match {
+    case i: Array[Int] => i map { e => e.toString }
+    case l: Array[Long] => l map { e => e.toString }
+    case d: Array[Double] => d map { e => e.toString }
+    case f: Array[Float] => f map { e => e.toString }
+    case c: Array[Char] => c map { e => e.toString }
+    case s: Array[Short] => s map { e => e.toString }
+    case b: Array[Byte] => b map { e => e.toString }
+    case z: Array[Boolean] => z map { e => e.toString }
+    case r: Array[AnyRef] => r map { e => e.toString }
+    case ar: AnyRef => throw new IllegalArgumentException(ar.getClass.getSimpleName + " cannot be printed as a table")
+  }
+
+  private def getTableColSizes(fields: Map[String,Any]) = {
+    val colSizes = new Array[Int](fields.size)
+
+    //Columns should be at least the size of the headers
+    var idx = 0
+    for(f <- fields.keys) {
+      colSizes(idx) = max(colSizes(idx), f.length + 2)
+      idx += 1
+    }
+    //columns should be at least the size of maximal element
+    idx = 0
+    for (f <- fields.keys) {
+      for (d <- arrayToString(fields(f))) {
+        colSizes(idx) = max(colSizes(idx), d.length + 2)
+      }
+      idx += 1
+    }
+
+    colSizes
+  }
+
   implicit def convertIterableToDataTable[T](i: Iterable[T]) : DataTable[T] = {
     if(i.isInstanceOf[DataTable[T]]) {
       i.asInstanceOf[DataTable[T]]
@@ -27,11 +145,11 @@ object DataTable {
       }
 
     }
-	else if(i.isInstanceOf[Grouping[_,T]]) {
-	  println("converting grouping to DataTable")
-	  val g = i.asInstanceOf[Grouping[_,T]]
-	  assert(g.elems.isInstanceOf[UnsafeArrayBuffer[T]])
-	  return new DataTable[T] {
+    else if(i.isInstanceOf[Grouping[_,T]]) {
+      println("converting grouping to DataTable")
+      val g = i.asInstanceOf[Grouping[_,T]]
+      assert(g.elems.isInstanceOf[UnsafeArrayBuffer[T]])
+      return new DataTable[T] {
 
         _data = g.elems.asInstanceOf[UnsafeArrayBuffer[T]]
 
@@ -39,11 +157,11 @@ object DataTable {
           throw new RuntimeException("Cannot add Record into a projected DataTable")
         }
       }
-	  
-	}
+
+    }
     else {
       val arrbuf = new UnsafeArrayBuffer[T]
-      
+
       for (e <- i) {
         arrbuf.append(e)
       }
@@ -55,7 +173,7 @@ object DataTable {
           throw new RuntimeException("Cannot add Record into a projected DataTable")
         }
       }
-	}
+    }
   }
 }
 
@@ -67,7 +185,7 @@ class UnsafeArrayBuffer[T] extends ArrayBuffer[T] {
     array = arr
     size0 = nsize
   }
-    
+
   def unsafeFillWithNull(initialSize:Int) {
     array = new Array(initialSize)
     size0 = initialSize
@@ -79,9 +197,9 @@ class DataTable[TSource](initialSize: Int) extends Iterable[TSource] with ppl.de
 
   println("initialSize : " + initialSize)
   var _data = new UnsafeArrayBuffer[TSource]
-  
+
   if(initialSize!=0) _data.unsafeFillWithNull(initialSize)
-  
+
   //ArrayBuffer.fill[TSource](initialSize)(null.asInstanceOf[TSource])
   println("size of internal _data is : " + _data.size)
   val grouped = false
@@ -95,9 +213,9 @@ class DataTable[TSource](initialSize: Int) extends Iterable[TSource] with ppl.de
   def dcSize = _data.size
   def unsafeSetData(arr: Array[TSource], size: Int) = _data.unsafeSetData(arr.asInstanceOf[Array[AnyRef]], size)
   def data = _data.getArray
-  
+
   def this() = this(0)
-	
+
 
   //TODO HCXXX: NEED TO REMOVE ALL THIS STUFF OR ADD IT TO DELITE COLLECTION
   def apply(idx: Int): TSource = _data(idx)
@@ -123,7 +241,7 @@ class DataTable[TSource](initialSize: Int) extends Iterable[TSource] with ppl.de
   implicit def cStrToDate(s: String) = Date(s)
 
 
-    def repeat(s: String, n:Int)(implicit sb: StringBuilder) {
+  def repeat(s: String, n:Int)(implicit sb: StringBuilder) {
     //assert(n < 0 || n > 300, "Incorrect value supplied for n in repeat")
     //todo for now, just ignore bad value of n
     if(n < 0 || n > 300)
@@ -258,8 +376,8 @@ class DataTable[TSource](initialSize: Int) extends Iterable[TSource] with ppl.de
 
   def forbid = throw new RuntimeException("Should not be using this method, got here by mistake")
   def notImplemented = throw new RuntimeException("Not Implemented Yet")
-  
-  
+
+
   //This part is hacked so I can run optiql in sequential mode
   def GroupBy[TKey](keySelector: TSource => TKey) = {
     //println("constructing hash-table for grouping operation")
@@ -273,7 +391,7 @@ class DataTable[TSource](initialSize: Int) extends Iterable[TSource] with ppl.de
     }
     result
   }
-  
+
   def Sum[@specialized T:Numeric](selector: TSource => T): T = {
     val n = implicitly[Numeric[T]]
     import n._
@@ -283,7 +401,7 @@ class DataTable[TSource](initialSize: Int) extends Iterable[TSource] with ppl.de
     }
     sum
   }
-  
+
   /*****
    * Internal Implementation functions
    */
