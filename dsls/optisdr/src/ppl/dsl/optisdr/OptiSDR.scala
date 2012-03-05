aa749605e02d8ab8ffac669d85d297435c499772
Michael Wu <mikemwu@stanford.edu>
2012-03-05 00:43:19 -0800

    made to extend optila

--- dsls/optisdr/src/ppl/dsl/optisdr/OptiSDR.scala
+++ dsls/optisdr/src/ppl/dsl/optisdr/OptiSDR.scala
@@ -3,43 +3,60 @@ package ppl.dsl.optisdr
 import scala.virtualization.lms.common._
 import ppl.delite.framework.{Config, DeliteApplication}
 
+import ppl.dsl.optila.{OptiLAApplication}
+import ppl.dsl.optila.{OptiLAScalaOpsPkg, OptiLAScalaOpsPkgExp, OptiLA, OptiLAExp, OptiLACompiler, OptiLALift, OptiLAUtilities}
+import ppl.dsl.optila.{OptiLAScalaCodeGenPkg, OptiLACudaCodeGenPkg, OptiLAOpenCLCodeGenPkg, OptiLACCodeGenPkg, OptiLACodeGenBase, OptiLACodeGenScala, OptiLACodeGenCuda, OptiLACodeGenOpenCL, OptiLACodeGenC}
+
 import ppl.dsl.optisdr.primitive._
 
 trait OptiSDRApplicationRunner extends OptiSDRApplication with DeliteApplication with OptiSDRExp
 
-trait OptiSDRApplication extends OptiSDR with OptiSDRLift {
+trait OptiSDRApplication extends OptiLAApplication with OptiSDR with OptiSDRLift {
   var args: Rep[Array[String]]
   def main(): Unit
 }
 
-trait OptiSDRScalaOpsPkg extends Base
+trait OptiSDRScalaOpsPkg extends OptiLAScalaOpsPkg
   with Equal with IfThenElse with Variables with While with Functions
   with BooleanOps with PrimitiveOps with TupleOps
   
-trait OptiSDRScalaOpsPkgExp extends OptiSDRScalaOpsPkg with DSLOpsExp
-  with EqualExp with IfThenElseExp with VariablesExp with WhileExp with FunctionsExp
-  with BooleanOpsExp with PrimitiveOpsExp
+trait OptiSDRScalaOpsPkgExp extends OptiLAScalaOpsPkgExp with OptiSDRScalaOpsPkg
 
-trait OptiSDRLift extends LiftVariables with LiftEquals with LiftString with LiftBoolean with LiftPrimitives {
+trait OptiSDRScalaCodeGenPkg extends OptiLAScalaCodeGenPkg 
+  { val IR: OptiSDRScalaOpsPkgExp  }
+
+trait OptiSDRCudaCodeGenPkg extends OptiLACudaCodeGenPkg
+  { val IR: OptiSDRScalaOpsPkgExp  }
+
+trait OptiSDROpenCLCodeGenPkg extends OptiLAOpenCLCodeGenPkg
+  { val IR: OptiSDRScalaOpsPkgExp  }
+
+trait OptiSDRCCodeGenPkg extends OptiLACCodeGenPkg
+  { val IR: OptiSDRScalaOpsPkgExp  }
+
+/**
+ * These are the portions of Scala imported into OptiML's scope.
+ */
+trait OptiSDRLift extends OptiLALift {
   this: OptiSDR =>
 }
 
 /**
  * This the trait that every OptiSDR application must extend.
  */
-trait OptiSDR extends Variables with OptiSDRScalaOpsPkg
+trait OptiSDR extends OptiSDRScalaOpsPkg with OptiLA
   with ComplexOps {
 
   this: OptiSDRApplication =>
 }
 
 // these ops are only available to the compiler (they are restricted from application use)
-trait OptiSDRCompiler extends OptiSDR {
+trait OptiSDRCompiler extends OptiLACompiler with OptiSDR {
    
   this: OptiSDRApplication with OptiSDRExp =>
 }
 
-trait OptiSDRExp extends OptiSDRCompiler with OptiSDRScalaOpsPkgExp
+trait OptiSDRExp extends OptiLAExp with OptiSDRCompiler with OptiSDRScalaOpsPkgExp
   with ComplexOpsExp {
   this: DeliteApplication with OptiSDRApplication with OptiSDRExp => // can't be OptiSDRApplication right now because code generators depend on stuff inside DeliteApplication (via IR)
 }
