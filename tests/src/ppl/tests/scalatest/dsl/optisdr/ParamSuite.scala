00149c6d887dde77cd76bd877a8dfcdeeb07a17b
Michael Wu <mikemwu@stanford.edu>
2012-04-11 13:12:10 -0700

    i can make belongs to nodes!

--- tests/src/ppl/tests/scalatest/dsl/optisdr/ParamSuite.scala
+++ tests/src/ppl/tests/scalatest/dsl/optisdr/ParamSuite.scala
@@ -14,16 +14,48 @@ import ppl.tests.scalatest._
  *
  */
 
-object EnumRangeTestRunner extends DeliteTestRunner with OptiSDRApplicationRunner with EnumRangeTest
-trait EnumRangeTest extends DeliteTestModule with OptiSDRApplication {
+object RangeTestRunner extends DeliteTestRunner with OptiSDRApplicationRunner with RangeTest
+trait RangeTest extends DeliteTestModule with OptiSDRApplication {
   def main() = {
+    // Make ranges. Check if lifting works
+    val a = Range(2, 10)
+    
+    // Make sure Range can extract constants!
+    val b = unit(3) + 5
+    val c = Range(b, 20)
+  }
+}
+
+object EnumTestRunner extends DeliteTestRunner with OptiSDRApplicationRunner with EnumTest
+trait EnumTest extends DeliteTestModule with OptiSDRApplication {
+  def main() = {
+    // Check if lifting works
     val a = ParamEnum(1, 2, 3)
-	
-	val b = Range(2, 10)
+
+    // Make sure Range can extract constants!
+    val b = unit(3) + 5
+    val c = ParamEnum(b, 20)
+  }
+}
+
+object BelongsToTestRunner extends DeliteTestRunner with OptiSDRApplicationRunner with BelongsToTest
+trait BelongsToTest extends DeliteTestModule with OptiSDRApplication {
+  def main() = {
+    // Make Enums and params
+    val a = ParamEnum(1, 2, 3)
+
+    val b = Range(2, 5)
+    
+    // Make some kind of variable
+    val c = unit(1)
+    
+    // Make sure belongs to works
+    belongsto(c, a)
+    belongsto(c, b)
   }
 }
 
 class ParamSuite extends DeliteSuite {
-  def testEnumRange() { compileAndTest(EnumRangeTestRunner) }
+  def testRange() { compileAndTest(RangeTestRunner) }
 }
 
