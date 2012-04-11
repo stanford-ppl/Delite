e8a6b79ce359c090099d34bc1baeb768eb54c7f5
Michael Wu <mikemwu@stanford.edu>
2012-04-11 12:10:58 -0700

    stream offsets?

--- dsls/optisdr/src/ppl/dsl/optisdr/LanguageOps.scala
+++ dsls/optisdr/src/ppl/dsl/optisdr/LanguageOps.scala
@@ -6,6 +6,8 @@ import scala.virtualization.lms.internal.GenericFatCodegen
 import scala.virtualization.lms.common._
 import scala.reflect.SourceContext
 
+import ppl.dsl.optila.DenseVector
+
 trait LanguageOps extends Base {
   this: OptiSDR =>
   
@@ -25,6 +27,7 @@ trait LanguageOps extends Base {
   // assert multiple of? assert minimum?
   
   def belongsto[T:Manifest](x: Rep[T], r: Range[T])(implicit ctx: SourceContext) : Rep[Unit]
+  def belongsto[T:Manifest](x: Rep[T], r: Enum[T])(implicit ctx: SourceContext) : Rep[Unit]
   
   // Just syntax sugar!
   // What we expect is a block that returns a function. Not sure how to expect a return type of a function with any number of args...
@@ -32,6 +35,8 @@ trait LanguageOps extends Base {
   
   // I think we need to wrap kernels that don't take stream args with a function that does a mapping
   // Perhaps we can make people wrap it themselves with some keyword... wish there was a better way
+  
+  // Next and Prev (to access different parts of stream)
 }
 
 trait LanguageOpsExp extends LanguageOps with BaseFatExp with EffectExp {
@@ -65,9 +70,11 @@ trait LanguageOpsExp extends LanguageOps with BaseFatExp with EffectExp {
   
   def assertWidth[T:Manifest](x: Exp[T], width: Exp[Int])(implicit ctx: SourceContext) = reflectEffect(AssertWidth(x,width))
   
-  case class BelongsTo[T:Manifest](x: Exp[T], r: Range[T]) extends Def[Unit]
+  case class BelongsToRange[T:Manifest](x: Exp[T], r: Range[T]) extends Def[Unit]
+  case class BelongsToEnum[T:Manifest](x: Exp[T], r: Enum[T]) extends Def[Unit]
   
-  def belongsto[T:Manifest](x: Exp[T], r: Range[T])(implicit ctx: SourceContext) = reflectEffect(BelongsTo(x,r))
+  def belongsto[T:Manifest](x: Exp[T], r: Range[T])(implicit ctx: SourceContext) = reflectEffect(BelongsToRange(x,r))
+  def belongsto[T:Manifest](x: Exp[T], e: Enum[T])(implicit ctx: SourceContext) = reflectEffect(BelongsToEnum(x,e))
   
   // Does this only work for integers?
   def create_range[T:Manifest](lo: Exp[T], hi: Exp[T]) : Range[T] = {
@@ -86,12 +93,24 @@ trait LanguageOpsExp extends LanguageOps with BaseFatExp with EffectExp {
 	}) : _*)
   }
   
+  case class StreamOffset[T:Manifest](x: Exp[T], i: Int) extends Def[T]
+  
+  case class StreamOffsets[T:Manifest](x: Exp[T], is: Enum[T]) extends Def[DenseVector[T]]
+  
   // Kernel stuff
   // 
   
   // State stuff
   /*
   case class StateName(name: Exp[String])
+  
+  stream prevbits? nextbits?
+  
+  << should map to prev and next (bits?)
+  
+  also need stream chunk
+  
+  reverse should not be advised for stream
   */
 }
 
