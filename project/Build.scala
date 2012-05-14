586e6806fe28fa8f21a1649a0211194f17f893d6
Kevin Brown <kjbrown@tflop2.Stanford.EDU>
2012-05-14 14:24:09 -0700

    fixed sbt build

--- project/Build.scala
+++ project/Build.scala
@@ -23,6 +23,10 @@ object DeliteBuild extends Build {
     libraryDependencies += "org.scala-lang" % "scala-library" % virtScala,
     libraryDependencies += "org.scala-lang" % "scala-compiler" % virtScala,
     libraryDependencies += scalatest,
+    // used in delitec to access jars
+    retrieveManaged := true,
+    scalacOptions += "-Yno-generic-signatures",
+    scalacOptions += "-Yvirtualize"
   )
 
   val virtBuildSettings = virtBuildSettingsBase ++ Seq(
