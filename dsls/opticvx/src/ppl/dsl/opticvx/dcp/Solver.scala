23504c75eb08a6b094804ed39436c310d25c54ae
cdesa <chris.m.desa@gmail.com>
2012-11-04 18:55:23 -0800

    Creating the solver IR.

--- dsls/opticvx/src/ppl/dsl/opticvx/dcp/Solver.scala
+++ dsls/opticvx/src/ppl/dsl/opticvx/dcp/Solver.scala
@@ -3,5 +3,25 @@ package ppl.dsl.opticvx.dcp
 trait DCPSolver {
   self: DCPShape with DCPCone with DCPAlmap =>
 
+  case class Solver(
+    val input: Shape,
+    val memory: Seq[Shape],
+    val scalars: Int,
+    val code: Seq[SolverOp]
+  )
+  
+  trait SolverOp
+  
+  // Computes m_dst = m_dst * s_scale_dst + m_src * s_scale_src
+  case class SolverOpMMpy(
+    val input: Shape,
+    val memory: Seq[Shape],
+    val almap: Almap,
+    val m_src: Int,
+    val m_dst: Int,
+    val s_scale_src: Int,
+    val s_scale_dst: Int
+  )
+  
   
 }
