13465a2883118ff0beb9996e64b073935391c896
Christopher Aberger <christopheraberger@caberger.local>
2014-05-15 14:54:37 -0700

    Changed the number of dynamic chunks to be a function of loop size.

--- runtime/src/ppl/delite/runtime/codegen/kernels/scala/ScalaMultiLoopGenerator.scala
+++ runtime/src/ppl/delite/runtime/codegen/kernels/scala/ScalaMultiLoopGenerator.scala
@@ -253,8 +253,8 @@ class ScalaMultiLoopHeaderGenerator(val op: OP_MultiLoop, val numChunks: Int, va
   //add code to code generate an atomic integer method
   //you can pull and set from this 
   protected def writeSynchronizedOffset(){
-    println("Number of dynamic chunks: " + op.numDynamicChunks)
-    out.append("private val proposedNumberOfDynamicChunks = "+op.numDynamicChunks+"\n")
+    out.append("private val proposedNumberOfDynamicChunks = closure.loopSize/(40*"+numChunks+")+" +op.numDynamicChunks+ "\n")
+    out.append("println(\"numChunks: \" + proposedNumberOfDynamicChunks)\n")
     out.append("val numDynamicChunks = if(proposedNumberOfDynamicChunks <= "+numChunks+" || "+numChunks+" == 1 || closure.loopSize < proposedNumberOfDynamicChunks) "+numChunks+" else proposedNumberOfDynamicChunks\n")
     //out.append("println(\"numDynamicChunks: \" + numDynamicChunks)\n")
     out.append("private val offset = new AtomicInteger("+numChunks+")\n")
