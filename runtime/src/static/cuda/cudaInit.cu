6b2846327e4299841e3f6611f0a7f3372cdd9302
HyoukJoong Lee <hyouklee@stanford.edu>
2015-02-21 20:40:43 -0800

    Use polling instead of blocking synchronization (Some kepler devices suffer from power management)

--- runtime/src/static/cuda/cudaInit.cu
+++ runtime/src/static/cuda/cudaInit.cu
@@ -4,12 +4,15 @@
 extern "C" JNIEXPORT void JNICALL Java_ppl_delite_runtime_executor_CudaExecutionThread_initializeThread(JNIEnv* env, jobject obj, jint threadId, jint numThreads);
 
 JNIEXPORT void JNICALL Java_ppl_delite_runtime_executor_CudaExecutionThread_initializeThread(JNIEnv* env, jobject obj, jint threadId, jint numThreads) {
-	//chose device num
-  if(cudaSuccess != cudaSetDevice(threadId)) {
+  //chose device num
+  if(cudaSuccess != cudaSetDevice(0)) {
     printf("FATAL : GPU device could not be initialized. \n");	
     exit(1);
   }
-	//reset the device
+
+  // Using blocksynchronization slows down some kepler devices due to power management. Default is polling.
+  /*
+  //reset the device
   if(cudaSuccess != cudaDeviceReset()) {
     printf("FATAL : cudaDeviceReset() failed \n");	
     exit(1);
@@ -19,6 +22,8 @@ JNIEXPORT void JNICALL Java_ppl_delite_runtime_executor_CudaExecutionThread_init
     printf("FATAL : GPU device has crashed (cudaSetDviceFlags). \n");	
     exit(1);
   }
+  */
+
   //called to initialize the device (can take a while)
   if(cudaSuccess != cudaThreadSynchronize()) {
     printf("FATAL : GPU device has crashed (cudaThreadSynchronize). \n");	
