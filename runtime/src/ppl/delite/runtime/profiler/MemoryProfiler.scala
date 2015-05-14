f537f7bc7459780ce450e8ad74ba12e55871eedf
JithinThomas <jithinpt@gmail.com>
2015-05-13 17:41:38 -0700

    Checking in changes related to cache-access stats instrumentation and some minor changes

--- runtime/src/ppl/delite/runtime/profiler/MemoryProfiler.scala
+++ runtime/src/ppl/delite/runtime/profiler/MemoryProfiler.scala
@@ -7,21 +7,48 @@ import ppl.delite.runtime.Config
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.Stack
 
+class MemoryAccessStats(l2HitRatio: Double, l2Misses: Int, l3HitRatio: Double, l3Misses: Int, bytesReadFromMem: Double) {
+    var l2CacheHitRatio: Double = l2HitRatio
+    var l2CacheMisses: Int = l2Misses
+    var l3CacheHitRatio: Double = l3HitRatio
+    var l3CacheMisses: Int = l3Misses
+    var bytesReadFromMC: Double = bytesReadFromMem
+}
+
 object MemoryProfiler
 {
 	var threadCount = 0
   	var stats = new ArrayBuffer[Map[String, List[Long]]]()
   	var threadToCurrKernel = new ArrayBuffer[Stack[String]]()
   	var threadToId: Map[String, Int] = Map()
+	var memoryAccessStatsMaps = new ArrayBuffer[Map[String, ArrayBuffer[MemoryAccessStats]]]()
 
-	def initializeStats(numThreads: Int) = synchronized {
+	var cppTidStart = -1
+	var numCppThreads = 0
+	var numScalaThreads = 0
+	var numCudaThreads = 0
+	var numOpenCLThreads = 0
+
+	def initializeStats(numScala: Int, numCpp:Int, numCuda: Int, numOpenCL: Int) = synchronized {
+		val numThreads = numScala + numCpp + numCuda + numOpenCL
 		threadCount = numThreads
+
+		if (numCpp > 0) {
+			cppTidStart = numScala
+		}
+
+		numCppThreads = numCpp
+		numScalaThreads = numScala
+		numCudaThreads = numCuda
+		numOpenCLThreads = numOpenCL
+
 		for (i <- List.range(0, numThreads)) {
 		  //val threadName = "ExecutionThread-" + i
 		  val threadName = "ExecutionThread" + i
 		  threadToId += threadName -> i
 		  stats += Map[String, List[Long]]()
 		  threadToCurrKernel += new Stack[String]()
+		  memoryAccessStatsMaps += Map[String, ArrayBuffer[MemoryAccessStats]]() 
 		}
 
 		threadToId += "main" -> numThreads
@@ -29,11 +56,51 @@ object MemoryProfiler
 		threadToCurrKernel += new Stack[String]()
 	}
 
+	def addMemoryAccessStats(
+        sourceContext: String, threadId: Int,
+        l2CacheHitRatio: Double, l2CacheMisses: Int,
+        l3CacheHitRatio: Double, l3CacheMisses: Int,
+        bytesReadFromMC: Double): Unit = {
+  	  Predef.println("sourceContext: " + sourceContext)
+      val stats = new MemoryAccessStats( l2CacheHitRatio, l2CacheMisses, l3CacheHitRatio, l3CacheMisses, bytesReadFromMC );
+      //memoryAccessStatsMaps(threadId) += sourceContext -> stats
+      if (!memoryAccessStatsMaps(threadId).contains(sourceContext)) {
+		memoryAccessStatsMaps(threadId) += sourceContext -> new ArrayBuffer[MemoryAccessStats]()
+	  }
+
+	  memoryAccessStatsMaps(threadId)(sourceContext) += stats
+    }
+
+	/*
+	def printMemoryAccessStats(): Unit = {
+        Predef.println("Printing memory access stats")
+        Predef.println("=====================================")
+		Predef.println("Length of list: " + memoryAccessStatsMaps.length)
+        var tid = 0
+        for (m <- memoryAccessStatsMaps) {
+            Predef.println("Thread id: " + tid)
+            for (kv <- m) {
+                val s = kv._2
+                val l2HitRatio = Math.round(s.l2CacheHitRatio * 100)
+                val l3HitRatio = Math.round(s.l3CacheHitRatio * 100)
+                Predef.println("sourceContext: " + kv._1 + " => ( " + l2HitRatio + "%, " + s.l2CacheMisses + ", " + l3HitRatio + "%, " + s.l3CacheMisses + ", " + s.bytesReadFromMC + " bytes)")
+            }
+
+            tid = tid + 1
+        }
+    }
+	*/
+
+	def printMemoryAccessStats(): Unit = {
+		Predef.println("printMemoryAccessStats() has been disabled")
+	}
+
 	def clearAll() {
 		stats.clear()
 		threadToCurrKernel.clear()
 		threadToId = Map()
-		initializeStats(threadCount)
+		memoryAccessStatsMaps = new ArrayBuffer[Map[String, ArrayBuffer[MemoryAccessStats]]]()
+		initializeStats(numScalaThreads, numCppThreads, numCudaThreads, numOpenCLThreads)
 	}
 
 	/*
@@ -114,11 +181,12 @@ object MemoryProfiler
   	}
 
   	def dumpProfile(writer: PrintWriter) {
-  		emitMemProfileDataArrays(writer, stats)
+  		emitMemProfileDataArrays( writer )
+		emitMemAccessStats( writer )
   	}
 
-	def emitMemProfileDataArrays(writer: PrintWriter, stats: ArrayBuffer[Map[String, List[Long]]]) {
-		var aggrStats = aggregateStatsFromAllThreads(stats)
+	def emitMemProfileDataArrays(writer: PrintWriter) {
+		var aggrStats = aggregateStatsFromAllThreads()
 		
 		var tabs = "  "
 		var twoTabs = tabs + tabs
@@ -129,10 +197,81 @@ object MemoryProfiler
   		}
 
   		writer.println(twoTabs + "\"dummy\": 0")
-  		writer.print(tabs + "}")
+  		writer.println(tabs + "},\n")
   	}
 
-	def aggregateStatsFromAllThreads(stats: ArrayBuffer[Map[String, List[Long]]]): Map[String, List[Long]] = {
+	/*
+	def emitMemAccessStats( writer: PrintWriter) {
+		var tabs = "  "
+		var twoTabs = tabs + tabs
+		writer.println( tabs + "\"MemAccessStats\": {" )
+		var tid = 0
+
+		for (memAccessStats <- memoryAccessStatsMaps) {
+			if (tid != 0) {
+				writer.print(",\n")
+			}
+
+			writer.print( twoTabs + tid + " : {\n" )	
+			val str = memAccessStats.map(kv =>  {
+				val stats = kv._2
+				"\"" + kv._1 + "\": {" +
+					(stats.l2CacheHitRatio * 100).toInt + "%, " + stats.l2CacheMisses + ", " + 
+					(stats.l3CacheHitRatio * 100).toInt + "%, " + stats.l3CacheMisses + ", " +
+					stats.bytesReadFromMC + "}"
+			}).mkString(",\n")
+
+			writer.println(twoTabs + tabs + str)
+			writer.print( twoTabs + "}" )
+		
+			tid = tid + 1
+		}		
+
+		writer.println( tabs + "}" )
+	}
+	*/
+
+	def emitMemAccessStats( writer: PrintWriter ) {
+		if (numCppThreads > 0) {
+			val tabs = "  "
+			val twoTabs = tabs + tabs
+			val cppTids = List.range(cppTidStart, cppTidStart + numCppThreads)
+			val kernelToStats = memoryAccessStatsMaps(cppTidStart)
+
+			writer.println( tabs + "\"MemAccessStats\": {" )
+			var firstKernel = true
+			for (kv <- kernelToStats) {
+				val kernel = kv._1
+				val statsLst = kv._2
+
+				writer.println(twoTabs + "\"" + kernel  + "\": {")	
+
+				for (i <- List.range(0, statsLst.length)) {
+					val s = cppTids.map(tid => {
+						val stats = memoryAccessStatsMaps(tid)(kernel)(i)
+						"[" + tid + ", " +
+							(stats.l2CacheHitRatio * 100).toInt + ", " + stats.l2CacheMisses + ", " + 
+							(stats.l3CacheHitRatio * 100).toInt + ", " + stats.l3CacheMisses + ", " +
+							stats.bytesReadFromMC + "]"
+					}).mkString(",")
+
+					if (i > 0) {
+						writer.println(",")
+					} 
+
+					writer.print(twoTabs + tabs + "\"" + i + "\"" + ": [" + s + "]")
+				}	
+
+				writer.println("\n" + twoTabs + "},")		
+			}
+
+			writer.println(twoTabs + "\"dummy\": {}")
+			
+			writer.println( tabs + "}" )
+		}
+	}
+
+	def aggregateStatsFromAllThreads(): Map[String, List[Long]] = {
 		var aggrStats = Map[String, List[Long]]()
 		for (m <- stats) {
 			for (kv <- m) {
