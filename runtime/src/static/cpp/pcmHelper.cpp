880b883773d25c6e1fa4455e11db6687e219406f
JithinThomas <jithinpt@gmail.com>
2015-05-17 13:54:34 -0700

    Adding cpp files for PCM-related functions

---
+++ runtime/src/static/cpp/pcmHelper.cpp
@@ -0,0 +1,50 @@
+
+#include "cpucounters.h"
+#include "pcmHelper.h"
+#include <iostream>
+
+PCM* pcmInit() {
+	std::cout << "Calling pcmInit()" << std::endl;
+	PCM* m = PCM::getInstance();
+  	m->disableJKTWorkaround();
+  
+  	switch( m->program() ) { 
+      	case PCM::Success:
+			std::cout << "PCM Initialized" << std::endl;
+			break;
+  
+      	case PCM::PMUBusy:
+         	std::cout << "PCM::PMU Busy!" << std::endl;
+         	m->resetPMU();
+         	return NULL;
+  
+      	default:
+			return NULL;
+  	}
+
+	return m;
+}
+
+PCMStats* getPCMStats(SystemCounterState& before, SystemCounterState& after) {
+	struct PCMStats* stats = new PCMStats();
+	stats->l2CacheHitRatio = getL2CacheHitRatio( before, after );
+	stats->l3CacheHitRatio = getL3CacheHitRatio( before, after );
+	stats->l2Misses = getL2CacheMisses( before, after );
+	stats->l3Misses = getL3CacheMisses( before, after );
+	stats->bytesReadFromMC = getBytesReadFromMC( before, after );
+
+	return stats;
+}
+
+void printPCMStats(PCMStats* stats) {
+	std::cout
+		 << "L2 Hit Ratio: " << stats->l2CacheHitRatio << std::endl
+		 << "L2 Misses	 : " << stats->l2Misses << std::endl
+		 << "L3 Hit Ratio: " << stats->l3CacheHitRatio << std::endl
+		 << "L3 Misses	 : " << stats->l3Misses << std::endl
+		 << "Bytes Read from MC: " << stats->bytesReadFromMC << std::endl;
+}
+
+void pcmCleanup() {
+	PCM::getInstance()->cleanup();
+}
