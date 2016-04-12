ec6bfc7e535d7d69aee34715d159afe2f56bfa92
raghu <raghup17@gmail.com>
2016-04-12 11:49:29 -0700

    Add an initial generator (ExecutableGenerator) for MaxJ. This is based off of the GPU generator, and only contains stub code (no real codegen). Also add a MaxJ Sync generator that generates code for all Sync nodes.

---
+++ runtime/src/ppl/delite/runtime/codegen/MaxJExecutableGenerator.scala
@@ -0,0 +1,174 @@
+package ppl.delite.runtime.codegen
+
+import kernels.cpp.CppMultiLoopHeaderGenerator
+import ppl.delite.runtime.graph.ops._
+import ppl.delite.runtime.graph._
+import ppl.delite.runtime.scheduler.{OpList, PartialSchedule}
+import ppl.delite.runtime.{Config,Delite}
+import ppl.delite.runtime.graph.targets.{OS, Targets}
+import collection.mutable.{ArrayBuffer, HashSet}
+import sync._
+import ppl.delite.runtime.graph.DeliteTaskGraph
+
+/**
+ * MaxJ code generator
+ */
+trait MaxJExecutableGenerator extends ExecutableGenerator {
+  import CppResourceInfo._
+
+  def deviceTarget = Targets.MaxJ
+  def hostTarget = Targets.getHostTarget(deviceTarget)
+
+  protected val getterList: ArrayBuffer[String] = ArrayBuffer()
+  protected val available: ArrayBuffer[(DeliteOP,String)] = ArrayBuffer()
+
+  protected val hostGenerator: CppExecutableGenerator
+
+  /**
+   * Add a source to the companion compilation object (MaxJCompile)
+   */
+  protected def addSource(source: String) {
+    MaxJCompile.addSource(source, executableName)
+  }
+
+  protected[codegen] def writeHeader() {
+    hostGenerator.writeHeader()
+    out.append("#include <stdio.h>\n")
+    out.append("#include <stdlib.h>\n")
+    out.append("#include <stdint.h>\n")
+    out.append("#include <sys/time.h>\n")
+    out.append("#include <Maxfiles.h>\n")
+    out.append("#include <MaxSLiCInterface.h>\n")
+  }
+
+  protected[codegen] def declareGlobals() {
+  }
+
+  protected[codegen] def initializeGlobals() {
+  }
+
+  protected def writeMethodHeader() {
+    out.append("// writeMethodHeader();\n")
+    declareGlobals()
+    hostGenerator.declareGlobals()
+
+    initializeGlobals()
+    hostGenerator.initializeGlobals()
+  }
+
+  protected def writeMethodFooter() {
+    out.append("// writeMethodFooter();\n")
+    out.append("}\n")
+  }
+
+  protected def writeFooter() { }
+
+  /**
+   * [COMMENT TODO] What does this method do?
+   * MaxJ can only schedule SingleTasks with opName "Accel"
+   * Doing nothing in this method for MaxJ
+   */
+  protected def makeNestedFunction(op: DeliteOP) = op match {
+    case _ =>
+      out.append(s"""// MaxJExecutableGenerator::makeNestedFunction($op)""")
+      println("Cannot generate op" + op.id)
+  }
+
+  private def deref(o: DeliteOP, s: String):String = {
+    if (!isPrimitiveType(o.outputType(s))) "*"
+    else ""
+  }
+
+  protected[codegen] def writeFunctionCall(op: DeliteOP) {
+    if (op.scheduledOn(Targets.Cpp) && !op.isInstanceOf[OP_Nested]) {
+      hostGenerator.writeFunctionCall(op)
+    } else {
+      if (op.task == null) return //dummy op
+
+      for (i <- op.getInputs)
+        available += i
+      for (o <- op.getOutputs if op.outputType(o)!="Unit")
+        available += Pair(op,o)
+
+      op match {
+        case _:OP_Single =>
+          //TODO: enable singletask GPU kernels that has an output
+          assert(op.getOutputs.filter(o=>op.outputType(o)!="Unit").isEmpty)
+          out.append(s"""// MaxJ writeFunctionCall($op)""")
+          out.append(op.task)
+          val args = op.getInputs.map(i => deref(i._1,i._2) + getSymDevice(i._1,i._2))
+          out.append(args.mkString("(",",",");\n"))
+        case _ =>
+          sys.error(s"""ERROR: Unsupported op '$op' for MaxJ writeFunctionCall()""")
+      }
+    }
+  }
+
+  protected def writeOutputAlloc(op: DeliteOP) {
+    out.append(s"""// MaxJ writeOutputAlloc($op)""")
+  }
+
+  //TODO: Remove using getSymCPU
+  protected def getSymCPU(name: String): String = getSymRemote(null, name)
+
+  protected def getSymRemote(op: DeliteOP, name: String): String = {
+    "xC"+name
+  }
+
+  protected def getSymHost(op: DeliteOP, name: String): String = {
+    "xH"+name
+  }
+
+  protected def getSymDevice(op: DeliteOP, name: String): String = {
+    "xG"+name
+  }
+
+  override protected[codegen] def writeSyncObject() {  }
+
+  protected def isPrimitiveType(scalaType: String) = Targets.isPrimitiveType(scalaType)
+
+  protected def generateInputArgs(op: DeliteOP): String = {
+    op match {
+      case _ =>
+        op.getInputs.map(i => getSymDevice(i._1,i._2)).mkString(",")
+    }
+  }
+}
+
+class MaxJMainExecutableGenerator(val location: Int, val graph: DeliteTaskGraph)
+  extends MaxJExecutableGenerator with MaxJSyncGenerator {
+
+  protected val hostGenerator: CppExecutableGenerator = new CppMainExecutableGenerator(Targets.resourceIDs(Targets.Cpp).head, graph)
+  hostGenerator.out = out
+
+  def executableName(location: Int) = "Executable" + location
+
+  protected def syncObjectGenerator(syncs: ArrayBuffer[Send], target: Targets.Value) = {
+    target match {
+      case Targets.Scala => new ScalaMainExecutableGenerator(location, graph) with ScalaSyncObjectGenerator {
+        protected val sync = syncs
+        override def executableName(location: Int) = executableNamePrefix + super.executableName(location)
+      }
+      //case Targets.Cpp => new CppMainExecutableGenerator(location, graph) with CudaSyncObjectGenerator {
+      //  protected val sync = syncs
+      //  override def executableName(location: Int) = executableNamePrefix + super.executableName(location)
+      //}
+      case _ => throw new RuntimeException("Unknown Host type " + target.toString)
+    }
+  }
+}
+
+object MaxJExecutableGenerator {
+  val syncObjects = ArrayBuffer[String]()
+
+  def makeExecutables(schedule: PartialSchedule, graph: DeliteTaskGraph) {
+    for (sch <- schedule if sch.size > 0) {
+      val location = sch.resourceID
+      new MaxJMainExecutableGenerator(location, graph).makeExecutable(sch) // native execution plan
+    }
+  }
+
+  def clear() {
+    syncObjects.clear
+  }
+}
