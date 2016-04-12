e6676905b9814c4d1e42a25523e3d8bb577e2057
raghu <raghup17@gmail.com>
2016-04-12 12:03:30 -0700

    Override compile method to only generate files for MaxJ

--- runtime/src/ppl/delite/runtime/codegen/MaxJCompile.scala
+++ runtime/src/ppl/delite/runtime/codegen/MaxJCompile.scala
@@ -6,10 +6,30 @@ import ppl.delite.runtime.graph.targets.Targets
 import tools.nsc.io._
 
 object MaxJCompile extends CCompile {
+
   def target = Targets.MaxJ
-  override def ext = "maxj"
+  override def ext = ".cpp"
+
   protected def configFile = "MaxJ.xml"
-  protected def compileFlags: Array[String] = Array("")
-  protected def linkFlags: Array[String] = Array("")
+  protected def compileFlags = Array("")
+  protected def linkFlags = Array("")
+
+  /**
+   * Emits the generated code from sourceBuffer
+   * into files. The 'cacheRuntimeSources' method does this,
+   * but factoring this into a separate method to enhance readability
+   */
+  def generateFiles() {
+    cacheRuntimeSources(sourceBuffer.toArray)
+    sourceBuffer.clear()
+  }
+
+  /**
+   * Overriding compile method to only generate files,
+   * not do actual compilation, for MaxJ
+   */
+  override def compile() {
+    generateFiles()
+  }
 
 }
