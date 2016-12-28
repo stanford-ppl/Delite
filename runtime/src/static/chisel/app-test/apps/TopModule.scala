c2b794e79df01f241e2fa8bb57d5d0afc006cfec
mattfel1 <mattfel@stanford.edu>
2016-12-28 01:13:58 -0500

    add more to test

--- runtime/src/static/chisel/app-test/apps/TopModule.scala
+++ runtime/src/static/chisel/app-test/apps/TopModule.scala
@@ -6,7 +6,31 @@ import org.scalatest.Assertions._
 
 class TopModuleTests(c: TopModule) extends PeekPokeTester(c) {
 
+	val timeout = 300
+
+	step(1)
+	reset(1)
+
+	// Enable hw accel
+	poke(c.io.top_en, 1)
+
+	// Run hw accel
     step(200)
+
+    // Wait until done or timeout
+    var done = peek(c.io.top_done)
+    var numCycles = 0
+    while ((done != 1) & (numCycles < timeout)) {
+    	step(1)
+    	numCycles += 1
+    	done = peek(c.io.top_done)
+    }
+    // See if we had timeout or strange no-op run
+    if ( (numCycles > timeout) | (numCycles < 2) ) {
+      println("ERROR: Either timeout or did not run at all!")
+      expect(c.io.top_done, 999) // TODO: Figure out how to "expect" signals that are not hw IO
+    }
+
 
 }
 
