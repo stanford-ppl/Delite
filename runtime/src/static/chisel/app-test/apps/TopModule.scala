4422894737d8943363f1fd8a9d13cf47fb6641de
mattfel1 <mattfel@stanford.edu>
2017-01-25 12:08:50 -0800

    change app test interface

--- runtime/src/static/chisel/app-test/apps/TopModule.scala
+++ runtime/src/static/chisel/app-test/apps/TopModule.scala
@@ -7,47 +7,47 @@ import java.io._
 
 class TopModuleTests(c: TopModule, in: String, timeout: Int) extends PeekPokeTester(c) {
 
-  var offchipMem = List[BigInt]()
+  // var offchipMem = List[BigInt]()
 
-  def handleLoaders() {
-    (0 until numMemCtrls).foreach { i =>
-      val req = (peek(c.io.MemStreams.outPorts(i).receiveBurst) == 1)
-      val size = peek(c.io.MemStreams.outPorts(i).size).toInt
-      val base = peek(c.io.MemStreams.outPorts(i).base).toInt
-      val addr = peek(c.io.MemStreams.outPorts(i).addr).toInt
-      val par = c.io.MemStreams.inPorts(i).data.length
-      if (req) {
-        for (j <- 0 until size) {
-          (0 until par).foreach { k => poke(c.io.MemStreams.inPorts(i).data(k), addr-base+j+k) }  
-          poke(c.io.MemStreams.inPorts(i).valid, 1)
-          step(1)
-        }
-        poke(c.io.MemStreams.inPorts(i).valid, 0)
-        step(1)
-      }
-    }
-  }
+  // def handleLoaders() {
+    // (0 until numMemCtrls).foreach { i =>
+    //   val req = (peek(c.io.MemStreams.outPorts(i).receiveBurst) == 1)
+    //   val size = peek(c.io.MemStreams.outPorts(i).size).toInt
+    //   val base = peek(c.io.MemStreams.outPorts(i).base).toInt
+    //   val addr = peek(c.io.MemStreams.outPorts(i).addr).toInt
+    //   val par = c.io.MemStreams.inPorts(i).data.length
+    //   if (req) {
+    //     for (j <- 0 until size) {
+    //       (0 until par).foreach { k => poke(c.io.MemStreams.inPorts(i).data(k), addr-base+j+k) }  
+    //       poke(c.io.MemStreams.inPorts(i).valid, 1)
+    //       step(1)
+    //     }
+    //     poke(c.io.MemStreams.inPorts(i).valid, 0)
+    //     step(1)
+    //   }
+    // }
+  // }
 
-  def handleStorers() {
-    (0 until numMemCtrls).foreach { i =>
-      val req = (peek(c.io.MemStreams.outPorts(i).sendBurst) == 1)
-      val size = peek(c.io.MemStreams.outPorts(i).size).toInt
-      val base = peek(c.io.MemStreams.outPorts(i).base).toInt
-      val addr = peek(c.io.MemStreams.outPorts(i).addr).toInt
-      val par = c.io.MemStreams.outPorts(i).data.length
-      if (req) {
-        for (j <- 0 until size) {
-          poke(c.io.MemStreams.inPorts(i).pop, 1)
-          (0 until par).foreach { k => 
-            offchipMem = offchipMem :+ peek(c.io.MemStreams.outPorts(i).data(k)) 
-          }  
-          step(1)
-        }
-        poke(c.io.MemStreams.inPorts(i).pop, 0)
-        step(1)
-      }
-    }
-  }
+  // def handleStorers() {
+    // (0 until numMemCtrls).foreach { i =>
+    //   val req = (peek(c.io.MemStreams.outPorts(i).sendBurst) == 1)
+    //   val size = peek(c.io.MemStreams.outPorts(i).size).toInt
+    //   val base = peek(c.io.MemStreams.outPorts(i).base).toInt
+    //   val addr = peek(c.io.MemStreams.outPorts(i).addr).toInt
+    //   val par = c.io.MemStreams.outPorts(i).data.length
+    //   if (req) {
+    //     for (j <- 0 until size) {
+    //       poke(c.io.MemStreams.inPorts(i).pop, 1)
+    //       (0 until par).foreach { k => 
+    //         offchipMem = offchipMem :+ peek(c.io.MemStreams.outPorts(i).data(k)) 
+    //       }  
+    //       step(1)
+    //     }
+    //     poke(c.io.MemStreams.inPorts(i).pop, 0)
+    //     step(1)
+    //   }
+    // }
+  // }
 
   val args = if (in.trim().length() > 0) in.split(" ").toList else List()
   val startTime = System.currentTimeMillis
@@ -61,15 +61,16 @@ class TopModuleTests(c: TopModule, in: String, timeout: Int) extends PeekPokeTes
   (0 until args.length).foreach{ i => poke(c.io.ArgIn.ports(i), args(i).toInt) } // Poke inputs
 
   // Wait until done or timeout
-  val numMemCtrls = c.io.MemStreams.inPorts.length
+  val GeneratedPokerInstance = new GeneratedPoker(c)
   var done = peek(c.io.top_done)
   var numCycles = 0
   val stepSize = 50
   while ((done != 1) & (numCycles < timeout)) {
     step(stepSize)
 
-    handleLoaders()
-    handleStorers()
+    GeneratedPokerInstance.handleLoadStore()
+    // handleLoaders()
+    // handleStorers()
 
     now = System.currentTimeMillis
     numCycles += stepSize
@@ -86,7 +87,7 @@ class TopModuleTests(c: TopModule, in: String, timeout: Int) extends PeekPokeTes
   val result = if (c.io.ArgOut.ports.length > 0) { 
     List(peek(c.io.ArgOut.ports(0))) // Arg result
   } else { // Mem result
-    offchipMem
+    GeneratedPokerInstance.offchipMem
   }
 
   // println(s"Hardware result: $result")
